8c106f6ca173ea420169a67a329ac72648226b81
aroogit commit -m First <arek.dec@gmail.com>
2023-07-25 16:27:25 +0200

    fighting with overflow

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import Feed from "@components/Feed"
 
 const Home = () => {
   return (
-    <section className="w-full flex-center flex-col">
+    <section className="w-full flex-center flex-col px-4">
 
         <p className=' text-xl md:text-3xl font-inter font-light text-black/80 text-center z-1'>Odkrywaj i dziel się swoimi pomysłami na nowe rzeczowniki zbiorowe wśród zwierząt.</p>
         <Feed />
